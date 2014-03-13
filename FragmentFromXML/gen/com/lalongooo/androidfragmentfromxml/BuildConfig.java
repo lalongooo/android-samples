dba6bc9bcdb5a7d03cc44978b67dc03968a2ad95
Jorge Eduardo <sirekrey1@gmail.com>
2014-03-12 22:28:33 -0600

    Create android fragments from XML files. Initial commit

---
+++ FragmentFromXML/gen/com/lalongooo/androidfragmentfromxml/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.lalongooo.androidfragmentfromxml;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
