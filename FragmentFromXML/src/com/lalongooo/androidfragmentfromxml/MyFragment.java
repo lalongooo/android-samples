b0d46508ee06842b42f294411415a5fe97a35b7a
Jorge Lalo <hdez.jeduardo@gmail.com>
2014-08-25 23:59:23 -0500

    Updated the 'FragmentFromXML sample', now using the android.support.v4.app.Fragment

--- FragmentFromXML/src/com/lalongooo/androidfragmentfromxml/MyFragment.java
+++ FragmentFromXML/src/com/lalongooo/androidfragmentfromxml/MyFragment.java
@@ -1,12 +1,11 @@
 package com.lalongooo.androidfragmentfromxml;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MyFragment extends Fragment {
+public class MyFragment extends android.support.v4.app.Fragment {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
