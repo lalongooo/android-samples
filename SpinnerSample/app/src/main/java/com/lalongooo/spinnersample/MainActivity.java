dd27f453d847c99dc7477b2a4e29b856365c6068
Jorge Lalo <hdez.jeduardo@gmail.com>
2015-02-25 22:57:17 -0600

    Added missing spinner validations. Now there are missing the default items for each

--- SpinnerSample/app/src/main/java/com/lalongooo/spinnersample/MainActivity.java
+++ SpinnerSample/app/src/main/java/com/lalongooo/spinnersample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.lalongooo.spinnersample;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -25,6 +26,7 @@ import retrofit.client.Response;
 
 public class MainActivity extends ActionBarActivity {
 
+    private ProgressDialog pDlg;
     private Spinner spnState;
     private Spinner spnMunicipality;
     private Spinner spnLocality;
@@ -40,7 +42,6 @@ public class MainActivity extends ActionBarActivity {
         fillStates();
     }
 
-
     private void fillStates(){
 
         ArrayList<State> alStates = new ArrayList<>();
@@ -55,30 +56,36 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
+
                 State selectedState = (State)parent.getItemAtPosition(position);
                 if(selectedState.getId() != 0){
 
+                    showDialog(getString(R.string.please_wait), getString(R.string.main_loading_cities));
+
                     // Remove localities
-                    if (spnLocality.getAdapter() != null && spnLocality.getAdapter().getCount() > 0){
-                        spnLocality.setAdapter(null);
-                    }
+                    resetSpinner(spnLocality);
 
                     try {
                         InegiFacilRestClient.get().getCities(String.valueOf(selectedState.getId()), new Callback<List<City>>() {
                             @Override
                             public void success(List<City> cities, Response response) {
                                 spnMunicipality.setAdapter(new CitySpinnerBaseAdapter(getApplicationContext(), cities));
+                                hideDialog();
                             }
 
                             @Override
                             public void failure(RetrofitError error) {
                                 Log.d("An error occurred", "");
+                                hideDialog();
                             }
                         });
 
                     }catch (Exception ex){
                         Log.d("An error ocurred", ex.getMessage());
                     }
+                }else{
+                    resetSpinner(spnMunicipality);
+                    resetSpinner(spnLocality);
                 }
             }
 
@@ -93,22 +100,29 @@ public class MainActivity extends ActionBarActivity {
 
                 City selectedCity = (City) parent.getItemAtPosition(position);
                 if(position != 0){
+
+                    showDialog(getString(R.string.please_wait), getString(R.string.main_loading_localities));
+
                     try {
                         InegiFacilRestClient.get().getTowns(String.valueOf(selectedCity.getClaveEntidad()), String.valueOf(selectedCity.getClaveMunicipio()), new Callback<List<Town>>() {
                             @Override
                             public void success(List<Town> towns, Response response) {
                                 spnLocality.setAdapter(new TownSpinnerBaseAdapter(getApplicationContext(), towns));
+                                hideDialog();
                             }
 
                             @Override
                             public void failure(RetrofitError error) {
                                 Log.d("An error occurred", "");
+                                hideDialog();
                             }
                         });
 
                     }catch (Exception ex){
                         Log.d("An error ocurred", ex.getMessage());
                     }
+                }else{
+                    resetSpinner(spnLocality);
                 }
             }
 
@@ -124,4 +138,20 @@ public class MainActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+    private void showDialog(String title, String text) {
+        pDlg = ProgressDialog.show(this, title, text, true);
+    }
+
+    private void hideDialog() {
+        pDlg.dismiss();
+    }
+
+    private void resetSpinner(Spinner spinner){
+        // Remove localities
+        if (spinner.getAdapter() != null && spinner.getAdapter().getCount() > 0){
+            spinner.setAdapter(null);
+        }
+    }
+
 }
